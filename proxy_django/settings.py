e105825cbc0a57792c2245aa9eb0aa47bfb89d6e
rmendes1 <joaorenatomendes@gmail.com>
2022-03-06 21:55:15 -0300

    settings updated, test variables removed

--- proxy_django/settings.py
+++ proxy_django/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
