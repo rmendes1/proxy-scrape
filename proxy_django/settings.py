1c709921c5dcc939e9d12ac25c349dcff5a03e4d
rmendes1 <joaorenatomendes@gmail.com>
2022-03-06 22:54:36 -0300

    settings updated to work with dotenv

--- proxy_django/settings.py
+++ proxy_django/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+from dotenv import load_dotenv()
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #os.environ.get('SECRET_KEY')
-#SECRET_KEY = 'django-insecure-rxh+2ry=4tizo@htr7sf#m%04%3v+)l5fsfc-8o7$3n#ho6r&n'
+#'django-insecure-rxh+2ry=4tizo@htr7sf#m%04%3v+)l5fsfc-8o7$3n#ho6r&n'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
