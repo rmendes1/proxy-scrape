1ebb35916a5ae6f305acc76b6ce800a4ee29d227
rmendes1 <joaorenatomendes@gmail.com>
2022-03-06 22:56:46 -0300

    load_dotenv import fixed

--- proxy_django/settings.py
+++ proxy_django/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-from dotenv import load_dotenv()
+from dotenv import load_dotenv
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
