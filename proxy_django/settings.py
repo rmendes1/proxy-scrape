cd0e47cee88f7b655d603a6a3ecc20a56b4db37a
rmendes1 <joaorenatomendes@gmail.com>
2022-03-06 22:44:56 -0300

    key removed

--- proxy_django/settings.py
+++ proxy_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #os.environ.get('SECRET_KEY')
-SECRET_KEY = 'django-insecure-rxh+2ry=4tizo@htr7sf#m%04%3v+)l5fsfc-8o7$3n#ho6r&n'
+#SECRET_KEY = 'django-insecure-rxh+2ry=4tizo@htr7sf#m%04%3v+)l5fsfc-8o7$3n#ho6r&n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
